b5752d895251ea2d8c56e8d329a696fc96093d60
uday mishra <mishrauday138@gmail.com>
2025-10-01 12:57:05 +0530

    My first test suite

---
+++ cypress/integration/examples/kod1.cy.js
@@ -0,0 +1,21 @@
+describe("Kod Landing First Test Suite", function() {
+
+    it("Landing Page", function() {
+
+
+        cy.visit("https://app.kodnest.com/login");
+
+        cy.get("#email-input").type("uday+kodd1@kodnest.com");
+
+        cy.get('[name="password"]').type("Uday@123");
+
+        cy.get('#login-submit-button').click();
+
+        // Wait for login to complete
+       cy.get('#profile-menu-item', { timeout: 10000 })
+            .should('be.visible')
+            .click();
+       
+
+    })
+})
